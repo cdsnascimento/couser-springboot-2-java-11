7e2d54e5877a566cf2f1a145d50d10d311754369
csnascimento <cs_nascimento@yahoo.com.br>
2021-02-14 01:12:52 -0300

    Revert "Product-OrderItem one-to-many association"
    This reverts commit 9d9a5efe73ee2447c4a4128ec42c60b6d219cc6c.

--- src/main/java/com/educandoweb/course/entities/pk/OrderItemPK.java
+++ src/main/java/com/educandoweb/course/entities/pk/OrderItemPK.java
@@ -22,7 +22,6 @@ public class OrderItemPK implements Serializable {
     @JoinColumn(name = "product_id")   
     private Product product;
 
-    
     public Order getOrder() {
         return order;
     }
@@ -31,7 +30,6 @@ public class OrderItemPK implements Serializable {
         this.order = order;
     }
 
-    
     public Product getProduct() {
         return product;
     }
