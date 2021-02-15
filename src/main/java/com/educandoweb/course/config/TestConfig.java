1877e552e0af6116a7033965cae9d15fb19ea605
csnascimento <cs_nascimento@yahoo.com.br>
2021-02-15 03:03:43 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.OrderItem;
+import com.educandoweb.course.entities.Payment;
 import com.educandoweb.course.entities.Product;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.entities.enums.OrderStatus;
@@ -74,7 +75,6 @@ public class TestConfig implements CommandLineRunner {
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
         userRepository.saveAll(Arrays.asList(u1, u2));
-
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 
         OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
@@ -83,6 +83,14 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
+
+
 
     }
 
--- src/main/java/com/educandoweb/course/repositories/OrderItemRepository.java
+++ src/main/java/com/educandoweb/course/repositories/OrderItemRepository.java
@@ -1,7 +1,5 @@
 package com.educandoweb.course.repositories;
 
-import java.util.List;
-
 import com.educandoweb.course.entities.OrderItem;
 
 import org.springframework.data.jpa.repository.JpaRepository;
